740cff74900b71419b1874de718c107facb1e7b9
Arne Siemens <arsiemens@gmail.com>
2019-08-18 15:32:44 +0200

    Vuex store with basic counter component

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Smt msg="awesome"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Smt from './components/Smt.vue'
+import { store } from "./store/store";
 
 export default {
   name: 'app',
+  store,
   components: {
-    HelloWorld
+    HelloWorld,
+    Smt
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style.css";
 </style>
---
+++ src/components/Smt.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="smt">
+    Smt: {{msg}}!
+    <p>[Store] Counter: {{count}}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Smt",
+  props: {
+    msg: String
+  },
+  computed: {
+    count() {
+      return this.$store.state.counter;
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+    decrement() {
+      this.$store.commit("decrement");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.smt {
+  border: 1px dashed darksalmon;
+}
+</style>
